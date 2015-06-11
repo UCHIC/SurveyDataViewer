69bf4e10aa4ded7b3c4b598b3f5f35567a0eda85
Juan <juan.caraballo17@gmail.com>
2015-06-11 10:27:57 -0600

    Added empty About and Contact pages.

--- src/SurveyDataViewer/urls.py
+++ src/SurveyDataViewer/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 
-from surveyinterface.views import HomeView, SurveyView, SurveyInfoView
+from surveyinterface.views import HomeView, SurveyView, SurveyInfoView, AboutView, ContactView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -14,6 +14,8 @@ urlpatterns = patterns('',
     url(r'^' + BASE_URL + 'survey/(?P<pk>\w+)$', SurveyView.as_view(), name='survey-view'),
     url(r'^' + BASE_URL + 'survey-info/(?P<pk>\w+)$', SurveyInfoView.as_view(), name='survey-info'),
     url(r'^' + BASE_URL + 'admin/', include(admin.site.urls)),
+    url(r'^' + BASE_URL + 'about/$', AboutView.as_view(), name='about-view'),
+    url(r'^' + BASE_URL + 'contact/$', ContactView.as_view(), name='contact-view'),
 )
 
 if not settings.DEPLOYED:

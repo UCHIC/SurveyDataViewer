c60d4dca3d26e113d66db192a31d69df0a4aa462
Juan <juan.caraballo17@gmail.com>
2020-05-20 10:05:12 -0400

    wrong variable name fix. again.

--- src/SurveyDataViewer/settings/linux_server.py
+++ src/SurveyDataViewer/settings/linux_server.py
@@ -7,9 +7,9 @@ DEPLOYED = True
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 if "host" in data:
-    ALLOWED_HOSTS.append(config["host"])
+    ALLOWED_HOSTS.append(data["host"])
 if "host_alt" in data:
-    ALLOWED_HOSTS.append(config["host_alt"])
+    ALLOWED_HOSTS.append(data["host_alt"])
 
 STATIC_ROOT = data["static_root"]
 STATIC_URL = data["static_url"]

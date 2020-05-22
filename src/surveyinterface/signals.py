5d0593cfb1f9b700f2bfc68c5e0dc67b98308aac
Juan <juan.caraballo17@gmail.com>
2020-05-22 15:09:17 -0400

    exception handling when deleting directory after deleting a survey.

--- src/surveyinterface/signals.py
+++ src/surveyinterface/signals.py
@@ -2,10 +2,14 @@ import os
 
 from django_cleanup.signals import cleanup_post_delete
 from django.dispatch import receiver
+from django.contrib import messages
 
 
 @receiver(cleanup_post_delete)
 def delete_folder(**kwargs):
     directory = os.path.join(kwargs['file'].storage.location, kwargs['file'].instance.code)
     if not os.listdir(directory):
-        os.rmdir(directory)
+        try:
+            os.rmdir(directory)
+        except OSError as ose:
+            messages.add_message(request, messages.WARNING, 'The directory for this survey couldn\'t be deleted')

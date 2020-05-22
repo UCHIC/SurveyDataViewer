e0102ff044b35ada537ed49fa23bca520d818b5a
Juan <juan.caraballo17@gmail.com>
2020-05-22 14:48:00 -0400

    modified url regex so routing works even when survey code has a dash (-).

--- src/SurveyDataViewer/urls.py
+++ src/SurveyDataViewer/urls.py
@@ -11,8 +11,8 @@ BASE_URL = settings.SITE_URL
 
 urlpatterns = patterns('',
     url(r'^' + BASE_URL + '$', HomeView.as_view(), name='home'),
-    url(r'^' + BASE_URL + 'survey/(?P<pk>\w+)$', SurveyView.as_view(), name='survey-view'),
-    url(r'^' + BASE_URL + 'survey-info/(?P<pk>\w+)$', SurveyInfoView.as_view(), name='survey-info'),
+    url(r'^' + BASE_URL + 'survey/(?P<pk>[-\w]+)$', SurveyView.as_view(), name='survey-view'),
+    url(r'^' + BASE_URL + 'survey-info/(?P<pk>[-\w]+)$', SurveyInfoView.as_view(), name='survey-info'),
     url(r'^' + BASE_URL + 'admin/', include(admin.site.urls)),
     url(r'^' + BASE_URL + 'about/$', AboutView.as_view(), name='about-view'),
     url(r'^' + BASE_URL + 'contact/$', ContactView.as_view(), name='contact-view'),
